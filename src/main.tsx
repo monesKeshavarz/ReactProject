18c3103a240a40e1db46eb5fd4a0b341568531ad
mones keshavarz <mones.keshavarz7878@gmail.com>
2023-04-30 21:54:00 +0430

    start project

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const App = React.createElement("div");
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(App);
